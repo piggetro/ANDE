459717d33fbf75f786560f9a57fb9d98b69ddb43
Zolastic <honhattienlul@gmail.com>
2023-11-19 23:33:19 +0800

    feat: added meditation button and pause circle

--- app/src/main/java/com/example/ande/MainActivity.java
+++ app/src/main/java/com/example/ande/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.ande;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.meditationButton) {
+
+            Intent intent = new Intent(this, MeditationPage.class);
+            startActivity(intent);
+        }
     }
 }
