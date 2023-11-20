f37156f96b91459d81fa5b893a2c3a88f9559798
Zolastic <honhattienlul@gmail.com>
2023-11-20 20:27:03 +0800

    feat: started on top part of the page

---
+++ app/src/main/java/com/example/ande/ThoughtsExpansionPage.java
@@ -0,0 +1,43 @@
+package com.example.ande;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+public class ThoughtsExpansionPage extends AppCompatActivity implements View.OnClickListener {
+
+    String[] sortByDropDownItems = {"Earliest", "Latest"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> sortByDropDownAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thoughts_expansion_page);
+
+//        autoCompleteTextView = findViewById(R.id.auto_complete_text_view);
+//        sortByDropDownAdapter = new ArrayAdapter<>(this, R.layout.list_item, sortByDropDownItems);
+//        autoCompleteTextView.setAdapter(sortByDropDownAdapter);
+//        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                String item = parent.getItemAtPosition(position).toString();
+//                Toast.makeText(ThoughtsExpansionPage.this, "Selected: " + item, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.thoughtsExpansionPageBackButton) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
