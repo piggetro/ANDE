cc075f9a20d645f1c9b424dc50ae7c97abd9093e
Kang Rui <tankangrui654321@gmail.com>
2023-12-30 15:46:32 +0800

    added login/signup page

--- app/src/main/java/com/example/ande/CalendarPage.java
+++ app/src/main/java/com/example/ande/CalendarPage.java
@@ -6,12 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class CalendarPage extends AppCompatActivity {
+public class CalendarPage extends AppCompatActivity implements  View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_page);
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.calendarBackButton) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else if (v.getId() == R.id.calendarThoughts || v.getId() == R.id.viewMoreCalendar) {
+            Intent intent = new Intent(this, ThoughtsExpansionPage.class);
+            startActivity(intent);
+        }
+    }
+
 
 }
